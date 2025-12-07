318024e37305bbf4e25e62ff68763edf2b70f05f
pfwashere <167692384+pfwashere@users.noreply.github.com>
2025-12-07 13:19:41 +0700

    Reformat ButtonUi.cs for consistency

--- ButtonUi.cs
+++ ButtonUi.cs
@@ -4,10 +4,11 @@ using UnityEngine.SceneManagement;
 public class Button : MonoBehaviour
 {
     public GameObject Canvas;
-    // Call this function to change the scene
+
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
         Destroy(Canvas);
     }
 }
+
